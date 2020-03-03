2b11540bb69b6862510d15261e2f4bd6baf00597
rainmaidou <48209136+rainmaidou@users.noreply.github.com>
2020-03-03 20:29:45 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -166,7 +166,7 @@ EOF
 	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.1/trojan-1.14.1-linux-amd64.tar.xz
 	tar xf trojan-1.*
 	#下载trojan客户端
-	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
+	wget https://github.com/rainmaidou/trojan/raw/master/trojan-cli.zip
 	unzip trojan-cli.zip
 	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
 	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
